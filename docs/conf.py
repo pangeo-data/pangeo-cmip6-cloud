8fbee7d2a78edc534e1d8794a84f94c5c7695b2c
Charles Blackmon-Luca <charlesbluca@gmail.com>
2020-06-09 16:56:31 -0400

    Add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'pangeo'
+html_favicon = '_static/favicon.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
