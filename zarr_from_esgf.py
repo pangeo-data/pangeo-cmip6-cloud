f786d6c1f3c91898cc6ebdda989a8f810ade2f42
Ryan Abernathey <ryan.abernathey@gmail.com>
2022-02-03 15:58:07 -0500

    broken!

--- zarr_from_esgf.py
+++ zarr_from_esgf.py
@@ -1,4 +1,19 @@
+import sys
+
 from mysearch import esgf_search # We probably want to strip this out later, left as is for now.
+
+DATASET_ID = sys.argv[1]
+
+facet_labels = ("mip_era", "activity_id", "institute_id", "source_id", "experiment_id", "member_id", "table_id", "variable_id", "grid_label", "version")
+
+facet_vals = DATASET_ID.split('.')
+if len(facet_vals) != 10:
+    raise ValueError(
+        "Please specify a query of the form "
+        '.'.join(facet_labels).upper()
+    )
+
+facets = dict(zip(facet_labels, facet_vals))
 
 node_dict = {'llnl' : "https://esgf-node.llnl.gov/esg-search/search",
          'ipsl' : "https://esgf-node.ipsl.upmc.fr/esg-search/search",
@@ -6,22 +21,11 @@ node_dict = {'llnl' : "https://esgf-node.llnl.gov/esg-search/search",
          'dkrz' : "https://esgf-data.dkrz.de/esg-search/search"}
 
 
-facet_dict = {
-                'table_id'      : 'Omon',
-                'activity_id'   : 'CMIP',
-                'experiment_id' : 'historical',
-                'variable_id'   : "so", 
-                'member_id'     : 'r1i1p1f1',
-                'source_id'     : 'CanESM5',
-                'grid_label'    : 'gn',
-            }
-
 search_node = 'llnl'
 ESGF_site = node_dict[search_node] # TODO: We might have to be more clever here and search through different nodes. For later.
 
 
-query_dict = {k:v for k,v in facet_dict.items()}
-df = esgf_search(facet_dict, server=ESGF_site) # this modifies the dict inside?
+df = esgf_search(facets, server=ESGF_site) # this modifies the dict inside?
 
 # get list of urls
 urls = df['url'].tolist()
