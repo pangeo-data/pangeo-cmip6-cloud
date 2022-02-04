f9b9c95d998ad552adadce815f9bf3e39a438ced
Ryan Abernathey <ryan.abernathey@gmail.com>
2022-02-03 23:13:21 -0500

    black

--- myconfig.py
+++ myconfig.py
@@ -3,30 +3,79 @@ import pandas as pd
 
 df_needed = pd.DataFrame()
 df_GCS = pd.DataFrame()
-fs = gcsfs.GCSFileSystem(token='anon',access='read_only',cache_timeout=-1)
+fs = gcsfs.GCSFileSystem(token="anon", access="read_only", cache_timeout=-1)
 
-#local_target_prefix = 'zarrs/'
-local_target_prefix = '/h112/naomi/zarr-minimal/'
-local_source_prefix = 'netcdfs/'
+# local_target_prefix = 'zarrs/'
+local_target_prefix = "/h112/naomi/zarr-minimal/"
+local_source_prefix = "netcdfs/"
 
-target_keys = ['activity_id','institution_id','source_id','experiment_id','member_id','table_id','variable_id','grid_label']
-target_keys2 = ['activity_id','institution_id','source_id','experiment_id','member_id','table_id','variable_id','grid_label','version']
+target_keys = [
+    "activity_id",
+    "institution_id",
+    "source_id",
+    "experiment_id",
+    "member_id",
+    "table_id",
+    "variable_id",
+    "grid_label",
+]
+target_keys2 = [
+    "activity_id",
+    "institution_id",
+    "source_id",
+    "experiment_id",
+    "member_id",
+    "table_id",
+    "variable_id",
+    "grid_label",
+    "version",
+]
 
-target_format = '%(' + ')s/%('.join(target_keys) + ')s'
-target_format2 = '%(' + ')s/%('.join(target_keys2) + ')s'
+target_format = "%(" + ")s/%(".join(target_keys) + ")s"
+target_format2 = "%(" + ")s/%(".join(target_keys2) + ")s"
 
-#target_prefix1 = 'gs://cmip6/'
-target_prefix2 = 'gs://cmip6/CMIP6/'
+# target_prefix1 = 'gs://cmip6/'
+target_prefix2 = "gs://cmip6/CMIP6/"
 
-node_pref = {'esgf-data1.llnl.gov':0,'esgf-data2.llnl.gov':0,'aims3.llnl.gov':0,'esgdata.gfdl.noaa.gov':10,'esgf-data.ucar.edu':10,
- 'dpesgf03.nccs.nasa.gov':5,'crd-esgf-drc.ec.gc.ca':6, 'cmip.bcc.cma.cn':10, 'cmip.dess.tsinghua.edu.cn':10,
- 'cmip.fio.org.cn':10, 'dist.nmlab.snu.ac.kr':10, 'esg-cccr.tropmet.res.in':10, 'esg-dn1.nsc.liu.se':10,
- 'esg-dn2.nsc.liu.se':10, 'esg.camscma.cn':10, 'esg.lasg.ac.cn':10, 'esg1.umr-cnrm.fr':10, 'esgf-cnr.hpc.cineca.it':10,
- 'esgf-data2.diasjp.net':10, 'esgf-data3.ceda.ac.uk':10, 'esgf-data3.diasjp.net':10, 'esgf-nimscmip6.apcc21.org':10, 'esgf-node2.cmcc.it':10, 'esgf.bsc.es':10, 'esgf.dwd.de':10, 'esgf.ichec.ie':10, 'esgf.nci.org.au':10, 'esgf.rcec.sinica.edu.tw':10, 'esgf3.dkrz.de':10, 'noresg.nird.sigma2.no':10, 'polaris.pknu.ac.kr':10, 'vesg.ipsl.upmc.fr':10}
+node_pref = {
+    "esgf-data1.llnl.gov": 0,
+    "esgf-data2.llnl.gov": 0,
+    "aims3.llnl.gov": 0,
+    "esgdata.gfdl.noaa.gov": 10,
+    "esgf-data.ucar.edu": 10,
+    "dpesgf03.nccs.nasa.gov": 5,
+    "crd-esgf-drc.ec.gc.ca": 6,
+    "cmip.bcc.cma.cn": 10,
+    "cmip.dess.tsinghua.edu.cn": 10,
+    "cmip.fio.org.cn": 10,
+    "dist.nmlab.snu.ac.kr": 10,
+    "esg-cccr.tropmet.res.in": 10,
+    "esg-dn1.nsc.liu.se": 10,
+    "esg-dn2.nsc.liu.se": 10,
+    "esg.camscma.cn": 10,
+    "esg.lasg.ac.cn": 10,
+    "esg1.umr-cnrm.fr": 10,
+    "esgf-cnr.hpc.cineca.it": 10,
+    "esgf-data2.diasjp.net": 10,
+    "esgf-data3.ceda.ac.uk": 10,
+    "esgf-data3.diasjp.net": 10,
+    "esgf-nimscmip6.apcc21.org": 10,
+    "esgf-node2.cmcc.it": 10,
+    "esgf.bsc.es": 10,
+    "esgf.dwd.de": 10,
+    "esgf.ichec.ie": 10,
+    "esgf.nci.org.au": 10,
+    "esgf.rcec.sinica.edu.tw": 10,
+    "esgf3.dkrz.de": 10,
+    "noresg.nird.sigma2.no": 10,
+    "polaris.pknu.ac.kr": 10,
+    "vesg.ipsl.upmc.fr": 10,
+}
 
 
-dtype = {'llnl' : "https://esgf-node.llnl.gov/esg-search/search",
-         'ipsl' : "https://esgf-node.ipsl.upmc.fr/esg-search/search",
-         'ceda' : "https://esgf-index1.ceda.ac.uk/esg-search/search",
-         'dkrz' : "https://esgf-data.dkrz.de/esg-search/search"}
-
+dtype = {
+    "llnl": "https://esgf-node.llnl.gov/esg-search/search",
+    "ipsl": "https://esgf-node.ipsl.upmc.fr/esg-search/search",
+    "ceda": "https://esgf-index1.ceda.ac.uk/esg-search/search",
+    "dkrz": "https://esgf-data.dkrz.de/esg-search/search",
+}
--- mysearch.py
+++ mysearch.py
@@ -11,15 +11,23 @@ import pandas as pd
 # API AT:
 # https://github.com/ESGF/esgf.github.io/wiki/ESGF_Search_REST_API#results-pagination
 
-def esgf_search(search, server="https://esgf-node.llnl.gov/esg-search/search",
-                files_type="HTTPServer", local_node=True,
-                project="CMIP6", page_size=500, verbose=False,
-                format="application%2Fsolr%2Bjson", toFilter=True):
+
+def esgf_search(
+    search,
+    server="https://esgf-node.llnl.gov/esg-search/search",
+    files_type="HTTPServer",
+    local_node=True,
+    project="CMIP6",
+    page_size=500,
+    verbose=False,
+    format="application%2Fsolr%2Bjson",
+    toFilter=True,
+):
 
     client = requests.session()
     payload = search
     payload["project"] = project
-    payload["type"]= "File"
+    payload["type"] = "File"
     if local_node:
         payload["distrib"] = "false"
 
@@ -44,7 +52,7 @@ def esgf_search(search, server="https://esgf-node.llnl.gov/esg-search/search",
 
         resp = resp["docs"]
         offset += len(resp)
-        #print(offset,numFound,len(resp))
+        # print(offset,numFound,len(resp))
         for d in resp:
             dataset_id = d["dataset_id"]
             dataset_size = d["size"]
@@ -52,10 +60,10 @@ def esgf_search(search, server="https://esgf-node.llnl.gov/esg-search/search",
                 sp = f.split("|")
                 if sp[-1] == files_type:
                     url = sp[0]
-                    if sp[-1] == 'OPENDAP':
-                        url = url.replace('.html', '')
+                    if sp[-1] == "OPENDAP":
+                        url = url.replace(".html", "")
                     dataset_url = url
-            all_frames += [[dataset_id,dataset_url,dataset_size]]
+            all_frames += [[dataset_id, dataset_url, dataset_size]]
 
     ddict = {}
     item = 0
@@ -63,41 +71,52 @@ def esgf_search(search, server="https://esgf-node.llnl.gov/esg-search/search",
         dataset_id = alist[0]
         dataset_url = alist[1]
         dataset_size = alist[2]
-        vlist = dataset_id.split('|')[0].split('.')[-9:]
-        vlist += [dataset_url.split('/')[-1]]
+        vlist = dataset_id.split("|")[0].split(".")[-9:]
+        vlist += [dataset_url.split("/")[-1]]
         vlist += [dataset_size]
         vlist += [dataset_url]
-        vlist += [dataset_id.split('|')[-1]]
+        vlist += [dataset_id.split("|")[-1]]
         ddict[item] = vlist
         item += 1
 
-    dz = pd.DataFrame.from_dict(ddict, orient='index')
+    dz = pd.DataFrame.from_dict(ddict, orient="index")
     if len(dz) == 0:
-       print('empty search response')
-       return dz
+        print("empty search response")
+        return dz
 
-    dz = dz.rename(columns={0: "activity_id", 1: "institution_id", 2:"source_id",
-                            3:"experiment_id",4:"member_id",5:"table_id",
-                            6:"variable_id",7:"grid_label",8:"version_id",
-                            9:"ncfile",10:"file_size",11:"url",12:"data_node"})
+    dz = dz.rename(
+        columns={
+            0: "activity_id",
+            1: "institution_id",
+            2: "source_id",
+            3: "experiment_id",
+            4: "member_id",
+            5: "table_id",
+            6: "variable_id",
+            7: "grid_label",
+            8: "version_id",
+            9: "ncfile",
+            10: "file_size",
+            11: "url",
+            12: "data_node",
+        }
+    )
 
-
-    dz['ds_dir'] = dz.apply(lambda row: target_format % row,axis=1)
-    dz['node_order'] = [node_pref[s] for s in dz.data_node ]
-    dz['start'] = [s.split('_')[-1].split('-')[0] for s in dz.ncfile ]
-    dz['stop'] = [s.split('_')[-1].split('-')[-1].split('.')[0] for s in dz.ncfile ]
-
+    dz["ds_dir"] = dz.apply(lambda row: target_format % row, axis=1)
+    dz["node_order"] = [node_pref[s] for s in dz.data_node]
+    dz["start"] = [s.split("_")[-1].split("-")[0] for s in dz.ncfile]
+    dz["stop"] = [s.split("_")[-1].split("-")[-1].split(".")[0] for s in dz.ncfile]
 
     if toFilter:
-       # remove all 999 nodes
-       dz = dz[dz.node_order != 999]
+        # remove all 999 nodes
+        dz = dz[dz.node_order != 999]
 
-       # keep only best node
-       dz = dz.sort_values(by=['node_order'])
-       dz = dz.drop_duplicates(subset =["ds_dir","ncfile","version_id"],keep='first')
+        # keep only best node
+        dz = dz.sort_values(by=["node_order"])
+        dz = dz.drop_duplicates(subset=["ds_dir", "ncfile", "version_id"], keep="first")
 
-       # keep only most recent version from best node
-       dz = dz.sort_values(by=['version_id'])
-       dz = dz.drop_duplicates(subset =["ds_dir","ncfile"],keep='last')
+        # keep only most recent version from best node
+        dz = dz.sort_values(by=["version_id"])
+        dz = dz.drop_duplicates(subset=["ds_dir", "ncfile"], keep="last")
 
     return dz
