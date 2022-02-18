6f3bd0ae99976a6803708b6f766913c95e2a2c8e
Julius Busecke <julius@ldeo.columbia.edu>
2022-02-18 18:28:34 -0500

    Update upload_dummy.py

--- upload_dummy.py
+++ upload_dummy.py
@@ -1,5 +1,5 @@
 import os
 import gcsfs
 # these env variables are set via the google auth github action?
-gcs = gcsfs.GCSFileSystem(project=os.environ["PROJECT_NAME"], bucket=os.environ["STORAGE_NAME"])
+gcs = gcsfs.GCSFileSystem(project='gcp-public-data-noaa-cmip6', bucket='cmip6')
 gcs.put_file('dummy.csv', 'cmip6/dummy.csv')
