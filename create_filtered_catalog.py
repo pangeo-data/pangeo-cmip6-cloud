db2e9001b03a81beb114febeeb2e8abe0a6b7994
Julius Busecke <julius@ldeo.columbia.edu>
2022-02-22 18:13:00 +0000

    refactor query out to module

--- create_filtered_catalog.py
+++ create_filtered_catalog.py
@@ -1,8 +1,7 @@
-import requests
-import json
 import gcsfs
 from tqdm.autonotebook import tqdm
 import pandas as pd
+from retractions import query_retraction
 
 
 # first get the total number of results
@@ -17,48 +16,7 @@ params = {
     "fields": "instance_id",
 }
 
-resp = requests.get(url=url, params=params)
-header = resp.json()  # Check the JSON Response Content documentation below
-n_items = header["response"]["numFound"]
-
-batchsize = 5000
-
-batches = range(0, n_items+1, batchsize)  # if I offset these, can
-params["limit"] = batchsize
-
-batch_jsons = []
-
-print("Downloading Retraction Data...")
-for batch in tqdm(batches):
-    params["offset"] = batch
-    resp = requests.get(url=url, params=params)
-    if resp.status_code != 200:
-        print(batch)
-        print(resp.status_code)
-    data = resp.json()
-    batch_jsons.append(data)
-
-# Convert to list of instance ids
-print("Extracting instance_ids...")
-all_retracted_instance_ids = []
-for data in tqdm(batch_jsons):
-    extracted = [i["instance_id"] for i in data["response"]["docs"]]
-    all_retracted_instance_ids.extend(extracted)
-
-# Fail out here if the total number of items is not what was promised in the header
-# I had a few instances today, where that was the case, I think a simple retry is
-# a good enough solution for now.
-n_retracted = len(all_retracted_instance_ids)
-if n_retracted == n_items:
-    print('Successfully downloaded all retraction info')
-else:
-    raise RuntimeError(f'Downloaded retraction info is incomplete. Found {n_retracted} items, expected {n_items}')
-
-# There is the possibility that we are getting duplicate instance_ids here because we query replicas
-# Make sure dubplicates are not carried forward
-retracted_instance_ids = set(all_retracted_instance_ids)
-print(f"{len(all_retracted_instance_ids)-len(retracted_instance_ids)} replicas found")
-
+retracted_instance_ids = query_retraction(url, params)
 
 # Now check which of the retracted datasets are in the pangeo catalog
 df_retracted_instance_ids = pd.Series(list(retracted_instance_ids)).to_frame(
