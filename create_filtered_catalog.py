efd5b8bea40fd4040b79f0dfe433686aaa2d366a
Julius Busecke <julius@ldeo.columbia.edu>
2022-02-23 13:00:49 -0500

    Update create_filtered_catalog.py

--- create_filtered_catalog.py
+++ create_filtered_catalog.py
@@ -7,7 +7,9 @@ from tqdm.autonotebook import tqdm
 from datetime import date
 from retractions import query_retraction_retry
 
-urls = [
+gcs = gcsfs.GCSFileSystem()
+catalog_url = "https://cmip6.storage.googleapis.com/pangeo-cmip6.csv"
+node_urls = [
 "https://esgf-node.llnl.gov/esg-search/search",
 # "https://esgf-data.dkrz.de/esg-search/search", #Currently having trouble. deactivate for testing.
 ]
@@ -25,7 +27,7 @@ params = {
 retracted_ids = {
     url :query_retraction_retry(
         url, params, batchsize=10000
-    ) for url in urls
+    ) for url in node_urls
 }
 
 # convert to pandas dataframes
@@ -33,12 +35,10 @@ retracted_ids_df = [pd.Series(list(v)).to_frame(name="instance_id") for v in ret
 
 # iteratively merge dataframes with 'outer' to get all possible retractions
 # from https://stackoverflow.com/a/44338256
-df_retracted_instance_ids = reduce(lambda  left,right: pd.merge(left,right,on=['instance_id'],how='outer'), retracted_ids_df)
+retracted_df = reduce(lambda  left,right: pd.merge(left,right,on=['instance_id'],how='outer'), retracted_ids_df)
 
 ## 
-catalog_url = "https://cmip6.storage.googleapis.com/pangeo-cmip6.csv"
 pangeo_df = pd.read_csv(catalog_url)
-
 
 print('Backing up catalog')
 local_filename = "local_catalog.csv"
@@ -60,11 +60,11 @@ pangeo_df["instance_id"] = pangeo_df["zstore"].apply(
     lambda x: ".".join(x.replace("gs://cmip6/", "").split("/")[0:-1])
 )
 
-df_to_remove = pangeo_df.merge(df_retracted_instance_ids, on="instance_id")
+df_to_remove = pangeo_df.merge(retracted_df, on="instance_id")
 print(f"Found {len(df_to_remove)} stores that need to be removed!")
 
 df_to_keep = pangeo_df.merge(
-    df_retracted_instance_ids, on=["instance_id"], how="left", indicator=True
+    retracted_df, on=["instance_id"], how="left", indicator=True
 )
 df_to_keep = df_to_keep[df_to_keep["_merge"] == "left_only"]
 
@@ -73,13 +73,6 @@ df_to_keep = df_to_keep.drop(columns=["_merge", "instance_id"])
 
 # Make sure that this did not loose/add entries
 assert len(df_to_keep) + len(df_to_remove) == len(pangeo_df)
-
-# Now write out the new catalog that does not contain the retraced dataset
-# for local testing
-# with open("/home/jovyan/keys/google_cmip6_service.json") as token_file:
-#     token = json.load(token_file)
-# gcs = gcsfs.GCSFileSystem(token=token)
-gcs = gcsfs.GCSFileSystem()
 
 # create local file
 df_to_keep.to_csv(local_filename, index=False)
