1a09484487c830ee7a4dba21668e49a989e0cf35
Julius Busecke <julius@ldeo.columbia.edu>
2022-03-02 18:15:45 -0500

    Update create_filtered_catalog.py

--- create_filtered_catalog.py
+++ create_filtered_catalog.py
@@ -11,7 +11,7 @@ gcs = gcsfs.GCSFileSystem()
 catalog_url = "https://cmip6.storage.googleapis.com/pangeo-cmip6.csv"
 node_urls = [
 "https://esgf-node.llnl.gov/esg-search/search",
-"https://esgf-data.dkrz.de/esg-search/search",
+# "https://esgf-data.dkrz.de/esg-search/search",
 "https://esgf-index1.ceda.ac.uk/esg-search/search",
 "https://esgf-node.ipsl.upmc.fr/esg-search/search",
 ]
